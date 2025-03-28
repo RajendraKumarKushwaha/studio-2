fdbe0611f4582737289ed6511ad115c409ba0f76
RajendraKumarKushwaha <rajendra.cs909@gmail.com>
2025-03-28 09:29:19 +0530

    text-center

--- src/component/BackgroundTextPage.jsx
+++ src/component/BackgroundTextPage.jsx
@@ -1,7 +1,7 @@
 export default function BackgroundTextPage({ text }) {
     return (
         <div className="h-80 mt-16 bg-[url('/images/two.jpg')] bg-[center_70%] bg-cover bg-no-repeat flex items-center justify-center text-white">
-            <h1 className="text-4xl para text-[#f28b00] font-bold">{text}</h1>
+            <h1 className="text-4xl text-center para text-[#f28b00] font-bold">{text}</h1>
         </div>
 
     );
